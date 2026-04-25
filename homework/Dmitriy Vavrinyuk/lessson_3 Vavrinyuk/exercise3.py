46b624b4c1c4d613853009f5f0ba41259ee87cf6
Dmitrtiy Vavrinyuk <sailar_salazatn@mail.ru>
2026-04-25 17:28:45 +0300

    Задание 3, арифметические операции

--- homework/Dmitriy Vavrinyuk/lessson_3 Vavrinyuk/exercise3.py
+++ homework/Dmitriy Vavrinyuk/lessson_3 Vavrinyuk/exercise3.py
@@ -3,10 +3,10 @@ from cmath import sqrt
 # среднее арифметическое
 x = 5
 y = 9
-c = (x + y)/2
+c = (x + y) / 2
 print(c)
 
 # среднее геометрическое
 z = 4
-w = pow((x * y * z), 1/3)
+w = pow((x * y * z), 1 / 3)
 print(w)
