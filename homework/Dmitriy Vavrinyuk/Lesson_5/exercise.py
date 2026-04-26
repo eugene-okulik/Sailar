295be330b013c6cc6b0edca21f395916bbe005b9
Dmitrtiy Vavrinyuk <sailar_salazatn@mail.ru>
2026-04-26 23:09:55 +0300

    Задание 5, форматирование

---
+++ homework/Dmitriy Vavrinyuk/Lesson_5/exercise.py
@@ -0,0 +1,36 @@
+# Задание 1
+
+person = ['John', 'Doe', 'New York', '+1372829383739', 'US']
+name, last_name, city, phone, country = person
+print(name, last_name, city, phone, country)
+
+
+# Задание 2
+
+list_1 = "результат операции: 42"
+list_2 = "результат операции: 514"
+list_3 = "результат работы программы: 9"
+
+lists = (list_1, list_2, list_3)
+result = int()
+
+for item in lists:
+    string_index = item.index(": ")
+    value_int = int(item[string_index + 1: ]) + 10
+    result += value_int
+print(result)
+
+# Задание 3
+
+students = ['Ivanov', 'Petrov', 'Sidorov']
+subjects = ['math', 'biology', 'geography']
+print("Students", (", ".join(students)), "study these subjects:", (", ".join(subjects)))
+
+students = ", ".join(students)
+subjects = ", ".join(subjects)
+
+my_text = 'Students {0} study these subjects: {1}'
+print(my_text.format(students, subjects))
+
+my_text = f"Students {students} study these subjects: {subjects}"
+print(my_text)
