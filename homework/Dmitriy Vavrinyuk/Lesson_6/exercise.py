62ea3beeb434e7da411a5073706fc53dfe592f08
Dmitrtiy Vavrinyuk <sailar_salazatn@mail.ru>
2026-04-27 21:20:33 +0300

    Задание 6, условия и циклы

---
+++ homework/Dmitriy Vavrinyuk/Lesson_6/exercise.py
@@ -0,0 +1,26 @@
+# Задание 1
+# Напишите программу, которая добавляет ‘ing’ в конец слов (к каждому слову) в тексте
+# “Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at,
+# dignissim vitae libero” и после этого выводит получившийся текст на экран. Знаки препинания не должны оказаться
+# внутри слова. Если после слова идет запятая или точка, этот знак препинания должен идти после того же слова,
+# но уже преобразованного.
+
+initial_sentences = ('Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae '
+                 'semper at, dignissim vitae libero')
+
+modife_senteces = []
+
+sentences = initial_sentences.split()
+for sentence in sentences:
+    if ',' in sentence:
+        sentence = sentence.replace(',', '')
+        sentence += "ing,"
+    elif '.' in sentence:
+        sentence = sentence.replace(',', '')
+        sentence += "ing."
+    else:
+        sentence += "ing"
+    modife_senteces += [sentence]
+
+modife_senteces = ' '.join(modife_senteces)
+print(modife_senteces)
