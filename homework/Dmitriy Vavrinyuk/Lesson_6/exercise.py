644787c18f3c48050299ebfc62c21837380e26a1
Dmitrtiy Vavrinyuk <sailar_salazatn@mail.ru>
2026-04-27 21:24:58 +0300

    Задание 6, условия и циклы

--- homework/Dmitriy Vavrinyuk/Lesson_6/exercise.py
+++ homework/Dmitriy Vavrinyuk/Lesson_6/exercise.py
@@ -5,8 +5,8 @@
 # внутри слова. Если после слова идет запятая или точка, этот знак препинания должен идти после того же слова,
 # но уже преобразованного.
 
-initial_sentences = ('Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae '
-                 'semper at, dignissim vitae libero')
+initial_sentences = ('Etiam tincidunt neque erat, quis molestie enim imperdiet vel. '
+                     'Integer urna nisl, facilisis vitae semper at, dignissim vitae libero')
 
 modife_senteces = []
 
