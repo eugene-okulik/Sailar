fd48efd11db8101d67b9a888249599bb826e1cab
Dmitrtiy Vavrinyuk <sailar_salazatn@mail.ru>
2026-09-13 17:01:32 +0300

    13 инструменты

--- homework/Dmitriy Vavrinyuk/lesson_13/exersice.py
+++ homework/Dmitriy Vavrinyuk/lesson_13/exersice.py
@@ -1,11 +1,3 @@
-# Нужно прочитать файлик, который лежит в репозитории в моей папке. Здесь: homework/eugene_okulik/hw_13/data.txt
-
-# Файлик не копируйте и никуда не переносите. Напишите программу, которая читает этот файл, находит в нём даты и
-# делает с этими датами то, что после них написано. Опирайтесь на то, что структура каждой строки одинакова:
-# сначала идет номер, потом дата, потом дефис и после него текст. У вас должен получиться код,
-# который находит даты и для даты под номером один в коде должно быть реализовано то действие, которое написано
-# в файле после этой даты. Ну и так далее для каждой даты.
-
 import datetime
 import os
 import re
@@ -14,9 +6,10 @@ import re
 # Подготовка переменных пути
 base_path = os.path.dirname(__file__)
 file_path = os.path.join(base_path, 'data.txt')
+
 homework_path = os.path.dirname(os.path.dirname(os.path.dirname(file_path)))
-test_path = os.path.join(homework_path, 'eugene_okulik\\hw_13', 'data.txt')
-# test_path_2 = 'C:\\Py projects\\Sailar\\homework\\eugene_okulik\\Lesson_13\\data.txt'
+test_path = os.path.join(homework_path, 'eugene_okulik', 'hw_13', 'data.txt')
+# test_path = base_path.parent.parent / 'eugene_okulik' / 'hw_13' / 'data.txt'
 print(f'Путь до папке где лежит файл - {test_path}')
 
 # Подготовка переменных времени
@@ -26,13 +19,6 @@ print(f'Текущее время - {today_midnight}')
 
 # Подготовка переменных
 PATTERN = r"(\d{4}.\d{2}.\d{2})\s*(\d{2}.\d{1,2}.\d{1,2}.\d{6})"
-
-
-# Поиск и чтение файла по строчкл
-def read_file():
-    with open(test_path, 'r') as data_file:
-        for line in data_file:
-            yield line
 
 
 #  Открытие файла по строчное сравнение по условию
@@ -58,10 +44,3 @@ with open(test_path, 'r', encoding='utf-8') as new_file:
                 dc = datetime.datetime.strptime(raw, "%Y-%m-%d %H:%M:%S.%f")
                 sub = now - dc
                 print(sub.days)
-
-# print(now)
-# today_midnight = datetime.datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
-# print(today_midnight)
-# after_midnight = now - today_midnight
-# print(after_midnight.seconds)
-# print(after_midnight)
